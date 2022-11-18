8fb570afe53c90a4e087b6e9583945c8aebe8a93
SI-W-00572\Si2001 <lorenzo.biraghi@si2001.it>
2022-11-18 15:32:02 +0100

    Security fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,21 +7,32 @@ import { DataComponent } from './pages/data/data.component';
 import { IndexComponent } from './pages/index/index.component';
 import { LoginComponent } from './pages/login/login.component';
 import { UsersComponent } from './pages/users/users.component';
+import { AuthJwtService } from './service/Auth/authJwt.service';
+import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LoginComponent },
-  { path: 'index', component: IndexComponent },
-  { path: 'data/users', component: UsersComponent },
-  { path: 'data/cars', component: CarsComponent },
-  { path: 'data/bookings', component: BookingsComponent },
-  { path: 'data', component: DataComponent },
-  { path: 'car/available', component: AvailableCarComponent },
+  { path: 'index', component: IndexComponent, canActivate: [AuthGuard] },
+  { path: 'data/users', component: UsersComponent, canActivate: [AuthGuard] },
+  { path: 'data/cars', component: CarsComponent, canActivate: [AuthGuard] },
+  {
+    path: 'data/bookings',
+    component: BookingsComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'data', component: DataComponent, canActivate: [AuthGuard] },
+  {
+    path: 'car/available',
+    component: AvailableCarComponent,
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard, AuthJwtService],
 })
 export class AppRoutingModule {}
